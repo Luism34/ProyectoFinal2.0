fdb6ee8e46a2c9715f8b9f50eeb7e03f6b8ff1cd
alumno <alumno@LAB7-25>
2017-05-19 09:35:45 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinal2.0/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ProyectoFinal2._0.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ProyectoFinal2.0/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoFinal2._0.Startup))]
+namespace ProyectoFinal2._0
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
